1a6f219808893df26eef21c75ebb8fd242305826
Gael <hectorfs2008@gmail.com>
2025-03-19 16:29:53 -0600

    Add files via upload

---
+++ PalomoProyecto2/script.js
@@ -0,0 +1,17 @@
+
+var boton = document.getElementById("miBoton");
+var boton2 = document.getElementById("miBoton2");
+var boton3 = document.getElementById("miBoton3");
+
+
+boton.addEventListener("click", function() {
+  window.open("Productos.html", "_blank");
+});
+
+boton2.addEventListener("click", function() {
+  window.open("Informacion.html", "_blank");
+});
+
+boton3.addEventListener("click", function() {
+  window.open("Compra.html", "_blank");
+});
